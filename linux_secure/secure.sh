41673cfb17f44da02769a0db93e1d31400fcd667
alvin <877856922@qq.com>
2017-12-01 13:59:06 +0800

    new change

--- linux_secure/secure.sh
+++ linux_secure/secure.sh
@@ -2,33 +2,37 @@
 #by alvin <zhuyuanwen@hoge.cn>
 
 ###############Password Security###############
+#备份文件
 a=`date +%Y%m%d`
-cp /etc/login.defs /m2odata/a.bak
+cp /etc/login.defs /m2odata/login.$a.bak
 
-#删除
-sed -i '/PASS_MAX_DAYS/'d  /etc/login.defs
-sed -i '/PASS_MIN_DAYS/'d  /etc/login.defs
-sed -i '/PASS_MIN_LEN/'d  /etc/login.defs
-sed -i '/PASS_WARN_AGE/'d /etc/login.defs
-#新增
-sed -i '20'a'PASS_MAX_DAYS\  \180' /etc/login.defs
-sed -i '21'a'PASS_MIN_DAYS\  \0' /etc/login.defs
-sed -i '22'a'PASS_MIN_LEN\  \12' /etc/login.defs
-sed -i '23'a'PASS_WARN_AGE\  \7' /etc/login.defs
+#替换
+sed -i '/^PASS_MAX_DAYS/ s/99999/180/g' 1.defs 
+sed -i '/^PASS_MIN_LEN/ s/5/12/g' 1.defs 
+
 #判断是否成功
 if [ $? = 0 ]
 then
-	echo "Password security is set Successfully!"
+	echo -e "\033[31m Password security is set Successfully! \033[0m"
 else
-	"Maybe there are some problems"
+	echo -e "\033[44;37;5m Maybe there are some problems!!! \033[0m"
 fi
 
 ###############Password Strength###############
+#备份文件
+cp  /etc/pam.d/system-auth-ac /m2odata/systemd-auth.$a.bak
+#删除两行
+sed -i '/password    requisite/d' /etc/pam.d/system-auth
+sed -i '/password    sufficient/d' /etc/pam.d/system-auth 
+#新增两行
+sed -i '/password    required/i password    requisite     pam_cracklib.so difok=3 minlen=12 ucredit=-1 lcredit=-1 dcredit=-1'  /etc/pam.d/system-auth
+sed -i '/password    required/i password    sufficient    pam_unix.so md5 shadow nullok try_first_pass use_authtok remember=5'  /etc/pam.d/system-auth
 
-sed -i '/password/'d  2.auth
-
-sed -i '14'a'password\   \ requisite\     \pam_cracklib.so\ \difok=3\ \minlen=12\ \ucredit=-1\ \lcredit=-1\ \dcredit=-1' 2.auth  
-sed -i '15'a'password\    \sufficient\    \pam_unix.so\ \md5\ \ shadow\ \nullok\ \try_first_pass\ \use_authtok\ \remember=5' 2.auth
-sed -i '16'a'password\     \required\      \pam_deny.so' 2.auth
-
-awk -F: '($2 == "") { print $1 }' /etc/shadow
+#检查是否存在空口令账号
+b=`awk -F: '($2 == "") { print $1 }' /etc/shadow`
+if [ $b="" ]
+then
+	echo "It's Good!"
+else
+	echo -e "\033[44;37;5m It's unsafety!!! \033[0m"
+fi
